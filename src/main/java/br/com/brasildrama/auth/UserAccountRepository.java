314d1ba9f169b4a14e52bf935d0d4f4767220ca0
ideiasboxsa <ideiasboxsa@gmail.com>
2026-08-24 21:12:42 -0300

    feat(auth): add user account repository

---
+++ src/main/java/br/com/brasildrama/auth/UserAccountRepository.java
@@ -0,0 +1,9 @@
+package br.com.brasildrama.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+
+interface UserAccountRepository extends JpaRepository<UserAccount, UUID> {
+    Optional<UserAccount> findByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCase(String email);
+}
