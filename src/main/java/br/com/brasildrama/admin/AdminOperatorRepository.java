190b089a24d3c06027703fa34b9a1f6535254026
ideiasboxsa <ideiasboxsa@gmail.com>
2026-08-25 12:04:49 -0300

    feat(ADM-002): add administrative operator repository

---
+++ src/main/java/br/com/brasildrama/admin/AdminOperatorRepository.java
@@ -0,0 +1,9 @@
+package br.com.brasildrama.admin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+interface AdminOperatorRepository extends JpaRepository<AdminOperator, UUID> {
+    Optional<AdminOperator> findByEmailIgnoreCase(String email);
+}
