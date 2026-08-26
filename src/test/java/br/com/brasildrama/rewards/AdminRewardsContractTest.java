6a31f7567d6ca0d9f6fdb58f3dd37e268249c49d
ideiasboxsa <ideiasboxsa@gmail.com>
2026-08-26 11:47:41 -0300

    test(rewards): execute admin economy aggregates

---
+++ src/test/java/br/com/brasildrama/rewards/AdminRewardsContractTest.java
@@ -0,0 +1,28 @@
+package br.com.brasildrama.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = {
+    "spring.liquibase.contexts=base,dev",
+    "security.jwt.secret=test-secret-for-brasil-drama-must-have-32-bytes"
+})
+class AdminRewardsContractTest {
+    @Autowired AdminRewardsApi rewards;
+
+    @Test
+    void aggregatesRewardEconomyFromMigratedDatabase() {
+        var result = rewards.overview();
+
+        assertThat(result).isNotNull();
+        assertThat(result.summary().usersWithRewards()).isGreaterThanOrEqualTo(0);
+        assertThat(result.summary().checkInsToday()).isGreaterThanOrEqualTo(0);
+        assertThat(result.summary().checkIns30d()).isGreaterThanOrEqualTo(result.summary().checkInsToday());
+        assertThat(result.rewardedAds().created()).isGreaterThanOrEqualTo(0);
+        assertThat(result.rewardedAds().claimed()).isGreaterThanOrEqualTo(0);
+        assertThat(result.missions()).isNotNull();
+    }
+}
